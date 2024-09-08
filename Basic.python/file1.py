de57a435bd31055e3cdcf6727a6c7930c9ff6c7a
Almizanshaikh <shaikalmizan49@gmail.com>
2024-09-08 21:54:06 +0530

    Improve

---
+++ Basic.python/file1.py
@@ -0,0 +1,14 @@
+a=2
+b=9
+c=5
+
+if a > b:
+    print ("a is greater than b")
+else:
+    print ("b is greater than a")
+    if b > c:
+        print ("b is greater than c")
+    else:
+        print ("c is greater than b")
+if a > b:
+    print ("a is greater than b")
