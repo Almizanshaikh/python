4fc2eed03bde5575f94d351d8dbea7960000990e
Almizanshaikh <shaikalmizan49@gmail.com>
2024-09-12 22:20:58 +0530

    Basic

---
+++ Basic.python/Variables.py
@@ -0,0 +1,23 @@
+
+# Variables
+
+# A variable is a name given to a memory location in a program.
+  
+name = "shaikh"  #string
+age = 23
+price = 20.3
+
+print("my name is :", name)
+print("my age is :", age)
+
+# firstprogram
+a = 5
+b = 20
+sum = (a+b)
+print(sum)
+
+a1 = 400
+b1 = 300
+diff = (a1-b1)
+print(diff)   
+
