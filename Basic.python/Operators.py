1a3ea018a49a1c36d510b3a96871a339ce25e5b3
Almizanshaikh <shaikalmizan49@gmail.com>
2024-09-13 23:00:30 +0530

    Basic

---
+++ Basic.python/Operators.py
@@ -0,0 +1,58 @@
+# arthametic operators 
+#(+,-,*,/,%,**)
+
+a = 5 
+b = 6
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) 
+print(a % b) #remainder
+print(a ** b) #a^b
+
+# relational/comparison operators
+#(==,!=,>,<,>=,<=)
+
+a1 = 50
+b2 = 60
+
+print(a1 == b2)  #false
+print(a1 != b2)  #True
+print(a1 >= b2)
+print(a1 <= b2)
+print(a1 > b2)
+
+# assigment operators
+#(=,+=,-=,*=,/=,%=,**=)
+
+num = 10
+num += 10
+print("num:",num)
+
+#logical operators
+#(not,and,or)
+b1 = 30
+a2 = 40
+
+print(not False)
+print(not(b1 > a2))
+
+val1 = True
+val2 = False
+print("AND operator:",val1 and val2)
+print("OR operator:",(b1 == a2 ) or (b1 > a2))
+
+# type conversion
+
+x = float("3")
+y = 4.45
+sum = x + y
+print(sum)
+
+x1 = int("9")
+y1 = 4.34
+sum = x1 + y1
+print(sum)
+
+
--- Basic.python/file1.py
+++ Basic.python/file1.py
@@ -10,9 +10,3 @@ else:
         print ("b is greater than c")
     else:
         print ("c is greater than b")
-<<<<<<< HEAD
-if a > b:
-    print ("a is greater than b")
-    
-=======
->>>>>>> 2f4c8c20b83d23d037e86d9b54c56d36b3d976ee
