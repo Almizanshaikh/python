2f1c994b94ec1ff3512d43a3802f4c1556a3c53e
Almizanshaikh <shaikalmizan49@gmail.com>
2024-09-14 23:46:18 +0530

    Basic

---
+++ Basic.python/Input.py
@@ -0,0 +1,21 @@
+# input() statement is used to accept values (using keyboard)from UserWarning
+
+# input() result for input()is always a str
+# int (input()) int
+# float (input()) float
+
+name = input("enter your name: ")
+print("welcome",name)    #welcome almizan
+
+aged = int(input("enter your age: "))
+print("your age is",aged)   #your age is 23
+
+int = ("88")
+val = float(input("enter some value: "))
+print(type(val),val)
+
+name = input("enter name: ")
+aged = int(input("enter age: "))
+marks = float(input("enter marks: "))
+
+print("welcome", name, "your age is",aged ,"your marks is",marks)
