dd0dc8ddae07d2ff0eaf4fd30604e23c9aac1d42
Almizanshaikh <shaikalmizan49@gmail.com>
2024-09-17 13:24:21 +0530

    Basic

---
+++ Fundamentals.py/Variables.py
@@ -0,0 +1,34 @@
+# In programming, a variable is a container (storage area) to hold data. For example,
+
+number = 10
+
+# Here, number is a variable storing the value 10.
+
+name = "almizan"
+print(name)  
+
+# In the above example, we assigned the value almizan to the name variable. Then, we printed out the value assigned to name
+
+# *Changing the Value of a Variable in Python
+
+name_1 = "almizan"
+print(name_1)
+
+name_1 = "shaikh"
+print(name_1)
+
+# Here, the value of site_name is changed from 'almizan' to 'shaikh'
+
+# *Assigning multiple values to multiple variables
+
+a,b,c, = 2,3,"hello"
+
+print(a)
+print(b)
+print(c)
+
+# If we want to assign the same value to multiple variables at once, we can do this as:
+
+class1 = class2 = "good class"
+print(class1)
+print(class2)
