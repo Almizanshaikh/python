efe5a25a274da2cb7b6143a26fedf1bea771ccf1
Almizanshaikh <shaikalmizan49@gmail.com>
2024-09-19 23:55:52 +0530

    Basic

---
+++ Fundamentals.py/Input and Output.py
@@ -0,0 +1,41 @@
+print("python is powerful")
+
+# output : python is powerful
+
+# Python Print Statement
+
+print("good morning!")
+print("it is rainy today")
+
+# Python print() with end Parameter
+
+print("good morning!", end= " ")
+print("i think today is a rainy day")
+
+# Python print() with sep parameter
+
+print("happy new year", 2025, "see you soon", sep= ". ")
+
+# Print Concatenated Strings
+
+print('python is a ' + 'awesome language.')
+
+# Output formatting
+# Sometimes we would like to format our output to make it look attractive. This can be done by using the str.format() method. For example,
+
+x = 10
+y = 20
+
+print(('the value of x is {} and y value is {}'.format(x,y)))
+
+# Python User Input
+
+# using input() to take user input  
+
+num1 = int(input('Enter first number: '))
+
+num2 = int(input('Enter second number: '))
+
+sum = num1+num2
+
+print(sum)
